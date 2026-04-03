c73c39f9093af62cd155d4162af303897651a5d8
harinicodez-git <24ucb115harini@kgkite.ac.in>
2026-04-04 01:38:38 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Enter Principal amount:"
+echo "Enter Principal:"
 read principal
 
-echo "Enter Rate of interest:"
+echo "Enter Rate of Interest:"
 read rate
 
-echo "Enter Time period:"
+echo "Enter Time Period:"
 read time
 
 si=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
